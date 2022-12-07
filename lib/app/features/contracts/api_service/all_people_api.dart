104f3ce4126d32cf7489372febf0efa8e8b208a4
Salih T <mhdsalih9656@gmail.com>
2022-12-07 23:44:16 +0530

    all_people_apiservice complteted

---
+++ lib/app/features/contracts/api_service/all_people_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:marlo_task/app/constants/api_endpoints.dart';
+import 'package:marlo_task/app/features/contracts/model/people_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetAllPeopleApi {
+ static Future<PeopleModel> getPeople() async {
+    try {
+      final response = await http
+          .get(Uri.parse(getAllPeopleUrl), headers: {'authtoken': token});
+      log(response.statusCode.toString());
+      if (response.statusCode == 200) {
+        return PeopleModel.fromJson(jsonDecode(response.body));
+      } else {
+        throw 'something went wrong !';
+      }
+    } catch (e) {
+      throw 'Something went wrong !';
+    }
+  }
+}
