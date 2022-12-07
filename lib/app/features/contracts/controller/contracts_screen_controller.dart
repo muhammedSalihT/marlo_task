fe54df9868ff15786b15cc29bec8a53636903145
Salih T <mhdsalih9656@gmail.com>
2022-12-07 23:45:13 +0530

    contracts_controller finished

---
+++ lib/app/features/contracts/controller/contracts_screen_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:marlo_task/app/features/contracts/api_service/all_people_api.dart';
+import 'package:marlo_task/app/features/contracts/model/people_model.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ContractsScreenController extends GetxController {
+  @override
+  void onInit() {
+    fetchAllPeople();
+    super.onInit();
+  }
+
+  List<PeopleModel> peopleList = [];
+
+  fetchAllPeople() async {
+    PeopleModel result = await GetAllPeopleApi.getPeople();
+
+    if (result.errorFlag == "SUCCESS") {
+      peopleList.add(result);
+      update();
+    } else {
+      Fluttertoast.showToast(
+        msg: result.message.toString(),
+      );
+    }
+  }
+}
