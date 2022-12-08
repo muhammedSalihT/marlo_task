437bda29521db7ed46d04e05b699616b1a5246b5
Salih T <mhdsalih9656@gmail.com>
2022-12-08 14:05:16 +0530

    constend complted

--- lib/app/constants/color_const.dart
+++ lib/app/constants/color_const.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color white = Colors.white;
+Color lightGrey = const Color(0xff161618);
 Color lightBlue = const Color(0xff0CABDF);
 Color grey = const Color(0xff75808A);
 Color darkBlue = const Color(0xff1A62C6);
@@ -8,3 +9,4 @@ Color darkCream = const Color(0xffAC816E);
 Color skyBlue = const Color(0xff0CABDF);
 Color scaffoldWhite = Colors.grey[100]!;
 Color dark = Colors.black;
+Color textFiledColor = const Color(0xffE9EEF0);
