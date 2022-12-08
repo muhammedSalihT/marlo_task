2cb939dfaab09c11d570ca7c2baf8e1adb0cd3e0
Salih T <mhdsalih9656@gmail.com>
2022-12-08 14:08:56 +0530

    main editted

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:marlo_task/app/constants/color_const.dart';
 import 'package:marlo_task/app/features/splash/view/splash_screen.dart';
+import 'package:marlo_task/app/theme/theme_controller.dart';
+import 'package:marlo_task/app/theme/theme_style.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final ThemeController _themeController = Get.put(ThemeController());
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          scaffoldBackgroundColor: scaffoldWhite,
-          appBarTheme: AppBarTheme(color: scaffoldWhite, elevation: 0)),
-      home: SplashScreen(),
-    );
+    return Obx(() {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: Themes.themeData(_themeController.darkTheme, context),
+        home: SplashScreen(),
+      );
+    });
   }
 }
