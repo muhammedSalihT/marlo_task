c63c7b229b695e20742cf2e8131bc65e01aa514e
Salih T <mhdsalih9656@gmail.com>
2022-12-08 14:07:43 +0530

    home completed

--- lib/app/features/home/controller/home_screen_conmtroller.dart
+++ lib/app/features/home/controller/home_screen_conmtroller.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
 
-class HomeScreeController extends GetxController{
-
+class HomeScreeController extends GetxController {
+  
 }
--- lib/app/features/home/view/home_screen.dart
+++ lib/app/features/home/view/home_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:marlo_task/app/features/home/controller/home_screen_conmtroller.dart';
+import 'package:marlo_task/app/theme/theme_controller.dart';
 
 class HomeScree extends GetWidget<HomeScreeController> {
-  const HomeScree({Key? key}) : super(key: key);
+  HomeScree({Key? key}) : super(key: key);
+
+  final themeController = Get.find<ThemeController>();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          themeController.selectTheme();
+        }, child: Obx(() {
+          return Text(
+              themeController.darkTheme.value ? "LightTheme" : "DarkTheme");
+        })),
+      ),
+    );
   }
 }
