fd5954fd172f6b569b4b352ad73fc7bce52e3e20
Salih T <mhdsalih9656@gmail.com>
2022-12-12 09:30:32 +0530

    color editted

--- lib/app/constants/color_const.dart
+++ lib/app/constants/color_const.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:marlo_task/app/theme/theme_controller.dart';
 
+final themeController = Get.find<ThemeController>();
 Color white = Colors.white;
+Color silverColor = const Color(0xff787F89);
+Color lightSilver = const Color(0xff232323);
 Color lightGrey = const Color(0xff161618);
 Color lightBlue = const Color(0xff0CABDF);
 Color grey = const Color(0xff75808A);
-Color darkBlue = const Color(0xff1A62C6);
+Color darkBlue = const Color(0x000cabdf);
 Color darkCream = const Color(0xffAC816E);
-Color skyBlue = const Color.fromARGB(255, 125, 181, 199);
+Color skyBlue = const Color.fromARGB(255, 189, 208, 215);
 Color scaffoldWhite = Colors.grey[100]!;
 Color dark = Colors.black;
-Color textFiledColor = const Color(0xffE9EEF0);
