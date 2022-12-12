1c2dee5598570f903db7f303ee874878fba99366
Salih T <mhdsalih9656@gmail.com>
2022-12-12 09:32:19 +0530

    invite_people_completed

--- lib/app/features/contracts/controller/invite_people_controller.dart
+++ lib/app/features/contracts/controller/invite_people_controller.dart
@@ -4,10 +4,20 @@ import 'package:get/get.dart';
 import 'package:marlo_task/app/features/contracts/api_service/invite_people_service.dart';
 import 'package:marlo_task/app/features/contracts/model/invite_people_model.dart';
 
+import '../../../utilities/internet_checking.dart';
+
 class InvitePeopleController extends GetxController {
   final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   String invitingRole = "Admin";
+  bool netCheck = false;
+
+  void disposeWidget() {
+    emailController.clear();
+    invitingRole = "Admin";
+    update();
+    Get.back();
+  }
 
   bool isValidEmail(String input) {
     return RegExp(
@@ -22,40 +32,53 @@ class InvitePeopleController extends GetxController {
     update();
   }
 
-  Future<void> inviteButtonPressed() async {
+  Future<String> inviteButtonPressed() async {
+    netCheck = true;
+    update();
     if (formKey.currentState!.validate()) {
       if (emailController.text.isEmpty) {
         Fluttertoast.showToast(
           msg: "Please enter email",
         );
-      } else {
-        final data = Datas(
-          email: emailController.text.trim(),
-          role: (roles.indexOf(invitingRole) + 1).toString(),
-        ).toJson();
-        InvitePeopleModel responce =
-            await InvitePeopleApi.postInvitePeople(data);
-
-        if (responce.message == "SUCCESS") {
-          Fluttertoast.showToast(
-            msg: responce.message.toString(),
-            toastLength: Toast.LENGTH_LONG,
-          );
-        } else {
-          Fluttertoast.showToast(
-            msg: responce.errorFlag.toString(),
-            toastLength: Toast.LENGTH_LONG,
-          );
-        }
+        netCheck = false;
+        update();
+        return "";
       }
     }
-  }
 
-  @override
-  void dispose() {
-    emailController.dispose();
-    invitingRole = "Admin";
-    update();
-    super.dispose();
+    bool network = await InternetConnectivity.checking();
+    if (network) {
+      final data = Datas(
+        email: emailController.text.trim(),
+        role: (roles.indexOf(invitingRole) + 1).toString(),
+      ).toJson();
+      InvitePeopleModel responce = await InvitePeopleApi.postInvitePeople(data);
+
+      if (responce.message == "SUCCESS") {
+        Fluttertoast.showToast(
+          msg: responce.message.toString(),
+          toastLength: Toast.LENGTH_LONG,
+        );
+        netCheck = false;
+        update();
+        return responce.message!;
+      } else {
+        Fluttertoast.showToast(
+          msg: responce.errorFlag.toString(),
+          toastLength: Toast.LENGTH_LONG,
+        );
+        netCheck = false;
+        update();
+        return "";
+      }
+    } else {
+      Fluttertoast.showToast(
+        msg: "please check your internet connectivity",
+        toastLength: Toast.LENGTH_LONG,
+      );
+      netCheck = false;
+      update();
+      return "";
+    }
   }
 }
